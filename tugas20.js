404ddd4d01362252e6610081913035ffdfafb911
hugoiman <hugogiman17@gmail.com>
2020-07-04 14:04:02 +0700

    Tugas 20 Javascript

---
+++ tugas20.js
@@ -0,0 +1,4 @@
+var text =
+  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+console.log(text.match("are"));
+console.log(text.match("words"));
